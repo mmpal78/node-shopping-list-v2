590b67960427dfa0b72532e51eac43cd34c40d39
Matt Palumbo <DukGwang@Matthews-iMac.local>
2017-03-22 10:00:01 -0400

    Added POST endpoint for /recipes.

--- server.js
+++ server.js
@@ -47,6 +47,23 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.post('/recipes', jsonParser, (req, res) => {
+  console.log(req.body);
+  const requiredFields = ['name', 'ingredients'];
+  for(let i = 0; i < requiredFields.length; i++){    
+    const field = requiredFields[i];
+    if(!(field in req.body)) {
+      const message = `Missing ${field} in req.body.`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+
 
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
